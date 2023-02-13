94061c685c6222d4f7ba74a9debce1ee5aeb08de
Fernanda Bess Gonçalves <fernanda-bess@hotmail.com>
2023-02-13 10:39:17 -0300

    fix counter bug

--- functions.js
+++ functions.js
@@ -1,18 +1,17 @@
-
-let contador = 1;
-
-let desejaContinuar;
+var desejaContinuar;
 
 
 do {
+    let contador = 1;
     let number = window.prompt("Digite um número de 1 a 9");
 if (number > 0 && number < 10) {
     while (contador <10) {
     console.log(number * contador)
     contador ++
-    }
-    desejaContinuar = window.prompt("Deseja continuar? [sim/nao]")
+    }  
+    desejaContinuar = window.prompt("Deseja continuar? [sim/nao]");
 } else {
    alert("Número inválido.")
 }
+
 } while (desejaContinuar === "sim")
